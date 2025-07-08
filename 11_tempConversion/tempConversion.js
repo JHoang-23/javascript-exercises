84c2876629bf07984bd794d6334131450efb42ab
John <johnhoang239@gmail.com>
2025-07-08 17:28:51 +0100

    Complete exercise 11_tempConversion
    Completed the exercise by implementing the formulas
    for fahrenheit to celsius and celsius to fahrenheit
    and using (Math.round(num) * 10)/10 to round to 1dp.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  //(x-32) * 5/9
+  let tempInCelsius = (tempInFahrenheit - 32) * (5/9)
+  let rounded = Math.round(tempInCelsius * 10) / 10
+
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  //x * 9/5 + 32
+  let tempInFahrenheit = (tempInCelsius * 9/5) + 32
+  let rounded = Math.round(tempInFahrenheit * 10) / 10
+
+  return rounded
+
 };
 
 // Do not edit below this line
