9e95f393e3379d36bf508747d16f37e6541e5062
John <johnhoang239@gmail.com>
2025-07-08 16:44:03 +0100

    Complete exercise 08_removeFromArray
    Completed the exercise by looping converting the
    args into a list using the rest operator (...) and
    looping through the array, checking if the array
    item is in the args list, if so skipping over it
    and pushing all other items into a new array.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let filteredArr = []
+    for (let i = 0; i < arr.length; i++){
+        if (!args.includes(arr[i])){
+            filteredArr.push(arr[i])
+        }
+    }
+    return filteredArr
+
 };
 
 // Do not edit below this line
