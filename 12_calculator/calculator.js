a34692acfaef6ca49a9d8039aa759f21d40f7769
John <johnhoang239@gmail.com>
2025-07-09 17:32:00 +0100

    Complete exercise 12_calculator
    Filled in each required function of the calculator
    with working logic to pass the tests.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(...args) {
+  let sum = 0;
+	for (let i = 0; i < args.length; i++){
+    sum += args[i]
+  }
+
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  let current = args[0]
+  for (let i = 1; i < args.length; i++){
+    current -= args[i]
+  }
+  return current
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++){
+    sum += arr[i]
+  }
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let sum = arr[0];
+	for (let i = 1; i < arr.length; i++){
+    sum = sum * arr[i]
+  }
 
+  return sum
 };
 
-const power = function() {
-	
+const power = function(...args) {
+  return args[0]**args[1]
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1
+  if (num == 0 || num == 1){
+    return total
+  }
+
+	for (let i = num; i > 1; i--){
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
