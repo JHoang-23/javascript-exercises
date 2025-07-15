9f691bb6de7a1a7e8b71ca86be60c6e70e46d797
John <johnhoang239@gmail.com>
2025-07-15 16:08:24 +0100

    Complete 14_Fibonnaci
    Completed the exercise by using a for loop to generate
    the fibonnaci sequence up to the given position
    and returning the value.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,37 @@
-const fibonacci = function() {
+// Create a function that returns a specific member of the Fibonacci sequence:
 
+// > A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
+// > In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+// > To learn more about Fibonacci sequences, go to: https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+const fibonacci = function(fibMember) {
+    // if (position < 0){
+    //     return "OOPS"
+    // }
+    // let fibonacciA = (1+Math.sqrt(5))**position
+    // let fibonacciB = (1-Math.sqrt(5))**position
+    // let bottom = (2**position)*(Math.sqrt(5))
+
+    // return Math.round((fibonacciA - fibonacciB)/bottom)
+    let position;
+    if (typeof fibMember !== "number") {
+        position = parseInt(fibMember)
+    }
+    else{ 
+        position = fibMember
+    }
+
+    if (position < 0){
+        return "OOPS"
+    } 
+
+    position = parseInt(position)
+    const fib = [0, 1]
+    for (let i = 2; i <= position; i++){
+        fib[i] = fib[i-1] + fib[i-2] 
+    }
+
+    return fib[position]
 };
 
 // Do not edit below this line
