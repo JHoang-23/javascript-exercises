4a00291a4663852b5bd1f53fa370585ddda23b43
John <johnhoang239@gmail.com>
2025-07-15 15:39:56 +0100

    Complete exercise 13_palindromes
    - Create a function that checks if palindrome.
    - Convert string to lowercase, check if it's alphanumeric.
    - Remove all non alphanumeric characters (e.g. punctuation).
    - Make a reverse of the string using .reverse
    - Compare the two strings, if they are same return true.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const alphanumerical = "0123456789abcdefghijklmnopqrstuvwxyz"
+const palindromes = function (string) {
 
+    //given a string that can contain spaces
+    //sanitise it first to remove spaces, punctuation etc.
+    let cleanString = string.
+    toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("")
+
+    return cleanString.split('').reverse().join('') == cleanString
 };
 
 // Do not edit below this line
